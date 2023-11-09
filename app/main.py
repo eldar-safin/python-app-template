0f66bd09d4ce8c40619c7c2422c2d60a08e488f3
Eldar Safin <git@eldar-s.online>
2023-11-09 14:02:37 +0300

    Добавил скелет приложения и очистку консоли перед запуском

---
+++ app/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+from settings import *
+from helpers import clear_console
+
+
+def main():
+    clear_console()    
+    
+    # Code
+
+
+if __name__ == '__main__':
+    sys.exit(main())
