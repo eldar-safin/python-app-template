8714fb881035af7b0b1a188e6b05e9301bdd25fa
Eldar Safin <git@eldar-s.online>
2023-11-17 10:55:08 +0300

    Изменил формат журналирования (добавил пробелы)

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
 class LogSettings:
     level = 'DEBUG'
-    format = '%(asctime)s | %(filename)s:%(lineno)d | %(levelname)8s | %(message)s'
+    format = '%(asctime)s | %(filename)16s:%(lineno)-4d | %(levelname)-8s | %(message)s'
     time_format = '%Y-%m-%d %H:%M:%S'
     filename = ''
     filemode = 'a'
