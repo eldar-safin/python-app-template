9b391fbe8170f8a45dc5950a47be1d6e09b1ff85
Eldar Safin <git@eldar-s.online>
2023-11-27 09:30:05 +0300

    Добавил настройкам функцию dump

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 
 import sys
 
+from settings import settings
 from logger import logger
 from helpers import clear_console
 
 
 def main():
     clear_console()
-    logger.debug('main')
+    logger.debug(f'Settings:\n{settings.dump()}')
 
 
 if __name__ == '__main__':
--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,16 @@
-class LogSettings:
+class BaseSettings:
+    def dump(self):
+        result = {}
+        for attr in filter(lambda v: not v.startswith('__'), vars(self.__class__)):
+            _value = self.__getattribute__(attr)
+            if BaseSettings in _value.__class__.__bases__:
+                result[attr] = _value.dump()
+            else:
+                result[attr] = _value
+        return result
+
+
+class LogSettings(BaseSettings):
     level = 'DEBUG'
     format = '%(asctime)s | %(filename)16s:%(lineno)-4d | %(levelname)-8s | %(message)s'
     time_format = '%Y-%m-%d %H:%M:%S'
@@ -6,7 +18,7 @@ class LogSettings:
     filemode = 'a'
 
 
-class Settings:
+class Settings(BaseSettings):
     log = LogSettings()
 
 
